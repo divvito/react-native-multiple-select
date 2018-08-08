80ee7937eebe4ca690d0641d8c879bd7473f2300
chris <chris@divvito.com>
2018-08-08 11:19:23 +1000

    Bump & build

--- built/lib/components/InputGroup.js
+++ built/lib/components/InputGroup.js
@@ -7,7 +7,7 @@ export default class InputGroup extends React.PureComponent {
         const { searchInputPlaceholderColor, indicatorOpen, inputGroupStyle, searchIconStyle, searchInputPlaceholderText, searchTerm, searchInputStyle, onChange, onAdd, } = this.props;
         return (React.createElement(View, { style: [styles.inputGroup, inputGroupStyle] },
             React.createElement(Icon, { name: "magnify", size: 20, color: colorPack.placeholderTextColor, style: [styles.searchIcon, searchIconStyle] }),
-            React.createElement(TextInput, { autoFocus: true, onChangeText: onChange, blurOnSubmit: false, onSubmitEditing: onAdd, placeholder: searchInputPlaceholderText, placeholderTextColor: searchInputPlaceholderColor, underlineColorAndroid: "transparent", style: [styles.searchInput, searchInputStyle], value: searchTerm }),
+            React.createElement(TextInput, { autoFocus: true, onChangeText: onChange, blurOnSubmit: true, onSubmitEditing: onAdd, placeholder: searchInputPlaceholderText, placeholderTextColor: searchInputPlaceholderColor, underlineColorAndroid: "transparent", style: [styles.searchInput, searchInputStyle], value: searchTerm, disableFullscreenUI: true }),
             indicatorOpen));
     }
 }
